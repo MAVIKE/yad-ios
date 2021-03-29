9fad23ad7f53c3714f45f24f1be343c6c9486403
Timonin Anton <dotruger.37@gmail.com>
2021-03-29 17:46:10 +0300

    Добавить файл

---
+++ ColossusClient.swift
@@ -0,0 +1,14 @@
+//
+//  ColossusClient.swift
+//  Colossus
+//
+//  Created by Антон Тимонин on 29.03.2021.
+//
+
+import Foundation
+
+final public class ColossusClient {
+    public static func hello() -> String {
+        return "Hello"
+    }
+}
