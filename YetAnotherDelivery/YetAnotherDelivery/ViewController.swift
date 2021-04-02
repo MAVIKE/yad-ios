2399ceaa833fbc31870deddbee8a4cfa619ea398
Shamil Imanov <1man0w25@gmail.com>
2021-04-02 22:33:05 +0300

    Добавил основу проекта

---
+++ YetAnotherDelivery/YetAnotherDelivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YetAnotherDelivery
+//
+//  Created by Shamil Imanov on 02.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
+    }
+
+
+}
+
