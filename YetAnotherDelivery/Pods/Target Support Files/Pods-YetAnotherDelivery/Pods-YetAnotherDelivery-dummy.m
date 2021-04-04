80b90736acfc5c34a3320706d90c594e203a01ed
Timonin Anton <dotruger.37@gmail.com>
2021-04-04 12:11:06 +0300

    Добавил зависимость - Alamofire

---
+++ YetAnotherDelivery/Pods/Target Support Files/Pods-YetAnotherDelivery/Pods-YetAnotherDelivery-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YetAnotherDelivery : NSObject
+@end
+@implementation PodsDummy_Pods_YetAnotherDelivery
+@end
--- YetAnotherDelivery/YetAnotherDelivery/ViewController.swift
+++ YetAnotherDelivery/YetAnotherDelivery/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        AF.request("https://httpbin.org/get").response { (data) in
+            print(data)
+            print("kek")
+        }
         view.backgroundColor = .purple
     }
 
